57ae1f59622b364e435142af8f7be3eb52fcc1e4
leosaq <leandrosaquisari7@gmail.com>
2026-06-30 12:50:40 -0500

    cambios

--- ejemplo7/proyectoDemo/negocio/forms.py
+++ ejemplo7/proyectoDemo/negocio/forms.py
@@ -2,21 +2,42 @@ from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
 from django import forms
 
-from negocio.models import Restaurante, Chef, Plato
+from negocio.models import Comentario, Restaurante, Chef, Plato
+
 
 class RestauranteForm(ModelForm):
     class Meta:
         model = Restaurante
-        fields = ['nombre', 'tipo_cocina', 'capacidad_meses']
+        fields = ["nombre", "tipo_cocina", "capacidad_meses"]
+
 
 class ChefForm(ModelForm):
     class Meta:
         model = Chef
-        fields = ['nombres', 'anios_experiencia', 'especialidad_culinaria',
-                  'restaurante']
+        fields = [
+            "nombres",
+            "anios_experiencia",
+            "especialidad_culinaria",
+            "restaurante",
+        ]
+
 
 class PlatoForm(ModelForm):
     class Meta:
         model = Plato
-        fields = ['nombre_plato', 'descripcion', 'precio_plato',
-                  'ingredientes_principales', 'chef']
+        fields = [
+            "nombre_plato",
+            "descripcion",
+            "precio_plato",
+            "ingredientes_principales",
+            "chef",
+        ]
+
+
+class ComentarioForm(ModelForm):
+    class Meta:
+        model = Comentario
+        fields = ["mensaje"]
+        labels = {
+            "mensaje": _("Ingrese su mensaje por favor"),
+        }
--- ejemplo7/proyectoDemo/negocio/models.py
+++ ejemplo7/proyectoDemo/negocio/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Restaurante(models.Model):
     nombre = models.CharField(max_length=100)
@@ -6,32 +8,53 @@ class Restaurante(models.Model):
     capacidad_meses = models.IntegerField(default=0)
 
     def __str__(self):
-        return "Restaurante: %s %s %d" % (self.nombre,
-                self.tipo_cocina,
-                self.capacidad_meses)
+        return "Restaurante: %s %s %d" % (
+            self.nombre,
+            self.tipo_cocina,
+            self.capacidad_meses,
+        )
 
 
 class Chef(models.Model):
     nombres = models.CharField(max_length=100)
     anios_experiencia = models.IntegerField(default=0)
     especialidad_culinaria = models.CharField(max_length=100)
-    restaurante = models.ForeignKey(Restaurante, on_delete=models.CASCADE,
-            related_name="chefs")
+    restaurante = models.ForeignKey(
+        Restaurante, on_delete=models.CASCADE, related_name="chefs"
+    )
 
     def __str__(self):
-        return "%s %d %s %s - (%s)" % (self.nombres, self.anios_experiencia,
-                                self.especialidad_culinaria, self.restaurante,
-                                self.restaurante.nombre)
+        return "%s %d %s %s - (%s)" % (
+            self.nombres,
+            self.anios_experiencia,
+            self.especialidad_culinaria,
+            self.restaurante,
+            self.restaurante.nombre,
+        )
+
 
 class Plato(models.Model):
     nombre_plato = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=100)
     precio_plato = models.FloatField()
     ingredientes_principales = models.CharField(max_length=200)
-    chef = models.ForeignKey(Chef, on_delete=models.CASCADE,
-            related_name="platos")
+    chef = models.ForeignKey(Chef, on_delete=models.CASCADE, related_name="platos")
 
     def __str__(self):
-        return "%s %s %.2f - (%s)" % (self.nombre_plato, self.descripcion,
-                                self.precio_plato,
-                                self.chef.nombres)
+        return "%s %s %.2f - (%s)" % (
+            self.nombre_plato,
+            self.descripcion,
+            self.precio_plato,
+            self.chef.nombres,
+        )
+
+
+class Comentario(models.Model):
+    mensaje = models.TextField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return "%s - (%s)" % (
+            self.mensaje,
+            self.usuario.username,
+        )
